1ad1890ed06b422d3122e167dd42d8fa07abfc99
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2016-05-17 16:23:19 +0200

    Adding GetStringSimilarityInPercent function and rewrite all other functions functionality

--- Tools/Tools/Extentions.cs
+++ Tools/Tools/Extentions.cs
@@ -21,21 +21,23 @@ namespace Tools
         public static bool IsNotBlank(this string str)
             => !string.IsNullOrEmpty(str) && str.Cast<char>().Any(ch => char.IsWhiteSpace(ch) == false);
 
-        public static long? ConvertToLongOrNull(this object ob)
+        public static long? ConvertToLongOrNull(this object data)
         {
+            if (IsBlank(data?.ToString())) return null;
             long result;
 
-            if (long.TryParse(ob.ToString(), NumberStyles.Any, CultureInfo.InvariantCulture, out result))
+            if (long.TryParse(data.ToString().Trim(), NumberStyles.Any, CultureInfo.InvariantCulture, out result))
                 return result;
 
             return null;
         }
 
-        public static int? ConvertToIntOrNull(this object ob)
+        public static int? ConvertToIntOrNull(this object data)
         {
+            if (IsBlank(data?.ToString())) return null;
             int result;
 
-            if (int.TryParse(ob.ToString(),NumberStyles.Any, CultureInfo.InvariantCulture, out result))
+            if (int.TryParse(data.ToString().Trim(),NumberStyles.Any, CultureInfo.InvariantCulture, out result))
                 return result;
 
             return null;
@@ -43,7 +45,7 @@ namespace Tools
 
         public static bool TryConvertStringToDateTime(ref DateTime objData, string sText)
         {
-            if (string.IsNullOrEmpty(sText)) return false;
+            if (IsBlank(sText)) return false;
 
             try
             {
@@ -64,7 +66,7 @@ namespace Tools
 
         public static decimal? ConvertToDecimalOrNull(this object data)
         {
-            if (string.IsNullOrEmpty(data?.ToString())) return null;
+            if (IsBlank(data?.ToString())) return null;
 
             decimal ret;
             if (decimal.TryParse(data.ToString(), NumberStyles.Any, CultureInfo.InvariantCulture, out ret))
@@ -72,11 +74,12 @@ namespace Tools
             return null;
         }
 
-        public static double? ConvertToDoubleOrNull(this object ob)
+        public static double? ConvertToDoubleOrNull(this object data)
         {
+            if (IsBlank(data?.ToString())) return null;
             double result;
 
-            if (double.TryParse(ob.ToString(), NumberStyles.Any, CultureInfo.InvariantCulture, out result))
+            if (double.TryParse(data?.ToString(), NumberStyles.Any, CultureInfo.InvariantCulture, out result))
                 return result;
 
             return null;
@@ -85,7 +88,7 @@ namespace Tools
         public static bool ConvertToBool(this object data)
         {
             var ret = false;
-            if (data != null)
+            if (IsNotBlank(data?.ToString()))
                 bool.TryParse(data.ToString(), out ret);
             return ret;
         }
@@ -105,5 +108,50 @@ namespace Tools
         }
 
         public static DateTime ConvertStringToDateTime(string dt)   => XmlConvert.ToDateTimeOffset(dt).DateTime;
+
+        public static short GetStringSimilarityInPercent(string first, string second, bool clearSpecSymbols)
+        {
+            if (IsBlank(first) && IsBlank(second)) return 100;
+            if (IsBlank(first) || IsBlank(second)) return 0;
+
+            if (clearSpecSymbols)
+            {
+                Regex rgx = new Regex("[^a-z0-9]");
+                first = rgx.Replace(first.ToLower(), "");
+                second = rgx.Replace(second.ToLower(), "");
+            }
+            else
+            {
+                first = first.ToLower().Trim();
+                second = second.ToLower().Trim();
+            }
+
+            double length = first.Length < second.Length ? first.Length : second.Length;
+            var sameLength = 0;
+
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                for (int j = 0; j < second.Length; j++)
+                {
+                    if (i >= first.Length)
+                        break;
+
+                    while (first[i] == second[j])
+                    {
+                        i++;
+                        j++;
+                        if (i >= first.Length || j >= second.Length)
+                            break;
+                        if (first[i] == second[j])
+                            sameLength++;
+                    }
+                }
+            }
+            if (sameLength != 0)
+                sameLength++;
+
+            return Convert.ToInt16(sameLength/length*100);
+        }
     }
 }
