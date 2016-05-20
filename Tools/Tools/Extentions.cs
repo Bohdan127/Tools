05d0de9dacd159fcb9ad5ea38dd7cc5a4d3185a4
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2016-05-20 19:15:45 +0200

    Make first nuget package with release version 1.0.0.4

--- Tools/Tools/Extentions.cs
+++ Tools/Tools/Extentions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Xml;
 
-namespace Tools
+namespace ToolsPortable
 {
     public static class Extentions
     {
--- Tools/Tools/Properties/AssemblyInfo.cs
+++ Tools/Tools/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tools")]
+[assembly: AssemblyTitle(".NET Tools Portable")]
 [assembly: AssemblyDescription("Tools library with many useful extensions for C# language, for quick import to any solution.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bohdan Benetskyi")]
-[assembly: AssemblyProduct("Tools")]
+[assembly: AssemblyProduct(".NET Tools Portable")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
