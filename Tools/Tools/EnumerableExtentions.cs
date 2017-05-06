68bd98454a685f3caccc42e4c8e1b4e05b0dfaaa
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2017-05-06 10:26:49 +0200

    Update to 1.0.0.15 Added Functional extensions

--- Tools/Tools/EnumerableExtentions.cs
+++ Tools/Tools/EnumerableExtentions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace ToolsPortable
 {
@@ -10,7 +11,8 @@ namespace ToolsPortable
     {
         [DebuggerStepThrough]
         [Pure]
-        public static TResult With<TInput, TResult>(this TInput input, Func<TInput, TResult> func)
+        public static TResult With<TInput, TResult>(
+            this TInput input, Func<TInput, TResult> func)
             where TResult : class
             where TInput : class
                 => input == null ? null : func(input);
@@ -46,8 +48,9 @@ namespace ToolsPortable
 
         [DebuggerStepThrough]
         [Pure]
-        public static IEnumerable<TInput> ForEach<TInput>(this IEnumerable<TInput> enumeration, Action<TInput> action)
-           where TInput : class
+        public static IEnumerable<TInput> ForEach<TInput>(
+            this IEnumerable<TInput> enumeration, Action<TInput> action)
+            where TInput : class
         {
             var enumerable = enumeration as IList<TInput> ?? enumeration.ToList();
             if (enumeration == null) return new TInput[0];
@@ -61,5 +64,42 @@ namespace ToolsPortable
         {
             yield return item;
         }
+
+        [DebuggerStepThrough]
+        [Pure]
+        public static TResult PipeTo<TSource, TResult>(
+            this TSource source, Func<TSource, TResult> func)
+            => func(source);
+
+        [DebuggerStepThrough]
+        [Pure]
+        public static TOutput Either<TInput, TOutput>(
+            this TInput input, Func<TInput, bool> condition,
+            Func<TInput, TOutput> ifTrue, Func<TInput, TOutput> ifFalse)
+            => condition(input) ? ifTrue(input) : ifFalse(input);
+
+        [DebuggerStepThrough]
+        [Pure]
+        public static TOutput Either<TInput, TOutput>(
+            this TInput input, Func<TInput, TOutput> ifTrue,
+            Func<TInput, TOutput> ifFalse)
+            => input.Either(x => x != null, ifTrue, ifFalse);
+
+        [DebuggerStepThrough]
+        [Pure]
+        public static TEntity ById<TKey, TEntity>(this IQueryable<TEntity> queryable, TKey id)
+            where TEntity : class, IHasId
+            where TKey : IComparable, IComparable<TKey>, IEquatable<TKey>
+            => queryable.SingleOrDefault(x => x.Id.Equals(id));
+
+        [DebuggerStepThrough]
+        [Pure]
+        public static TProjection ById<TKey, TEntity, TProjection>(
+            this IQueryable<TEntity> queryable, TKey id,
+            Expression<Func<TEntity, TProjection>> projectionExpression)
+            where TKey : IComparable, IComparable<TKey>, IEquatable<TKey>
+            where TEntity : class, IHasId
+            where TProjection : class, IHasId
+            => queryable.Select(projectionExpression).SingleOrDefault(x => x.Id.Equals(id));
     }
 }
---
+++ Tools/Tools/IHasId.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsPortable
+{
+    public interface IHasId
+    {
+        string Id { get; set; }
+    }
+}
